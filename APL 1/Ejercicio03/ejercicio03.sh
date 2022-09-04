974c106a6e5925371ae9f847582612063af7f84d
Lucas <80497029+Lev-93@users.noreply.github.com>
2022-09-04 18:42:13 -0300

    Añadiendo encabezado ejercicio 03

--- APL 1/Ejercicio03/ejercicio03.sh
+++ APL 1/Ejercicio03/ejercicio03.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-################################################################################
-##  Ejercicio nro 3 del APL 1 - 2c 2022 - Entrega nro 1
-##  Script: Ejercicio03.sh
-##
-##  Integrantes del grupo
-##  Villegas, Lucas Ezequiel 37792844
-################################################################################
+
+# =========================== Encabezado =======================
+
+# Nombre del script: Ejercicio4.sh
+# Número de ejercicio: 4
+# Trabajo Práctico: 1
+# Entrega: Primera entrega
+
+# ==============================================================
+
+# ------------------------ Integrantes ------------------------
+# 
+#	Nombre				|	Apellido			|	DNI
+#	Matías				|	Beltramone			|	40.306.191
+#	Eduardo				|	Couzo Wetzel			|	43.584.741
+#	Brian				|	Menchaca			|	40.476.567
+#	Ivana				|	Ruiz				|	33.329.371
+#	Lucas				|	Villegas			|	37.792.844
+# -------------------------------------------------------------
+
 nombreScript=$(readlink -f $0)
 dir_base=`dirname "$nombreScript"`
 
