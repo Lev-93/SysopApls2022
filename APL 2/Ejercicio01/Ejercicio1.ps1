16d0cd74884911412664b7189ed57f78e6efd1fc
EACW-UNLAM <111432181+EACW-UNLAM@users.noreply.github.com>
2022-09-25 15:08:38 -0300

    Update Ejercicio1.ps1

--- APL 2/Ejercicio01/Ejercicio1.ps1
+++ APL 2/Ejercicio01/Ejercicio1.ps1
@@ -16,8 +16,8 @@ Write-Host "El path no existe"
 }
 #1) El script tiene por objertivo mostrar en pantalla los N procesos (indicados en el parametro $cantidad) (Id y nombre) que utilizen 
 #   más de 100MB de me memoría. Además se escriben en un archvio de texto todos los procesos que usen más de 100MB de memoria
-#2) Se podria agrgar una validacion para asegurar que el parametro segundo parametro es la cantidad de proceso a mostrar en pantalla y 
-#   así ejecutar el Script sin usar el -cantidad
+#2) Se podria agregar una validacion para asegurar que el parametro segundo parametro es la cantidad de proceso a mostrar en pantalla y 
+#   así ejecutar el Script sin usar el -cantidad.
 #
 #3) Si se ejecuta sin ningun paramentro se inicializan los parametros por defecto ($pathSalida = ".\procesos.txt y $cantidad = 3). En caso
-#   de que no exista un archvio \procesos.txt se
+#   de que no exista un archvio \procesos.txt se informar que "El path no existe" y termina la ejecición del script.
